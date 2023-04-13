cf8d5b76ff89bf9b03de93c308f521080dba60b7
noiaibodullayeva <ibodullaevanoila07@gmail.com>
2023-04-13 22:37:49 +0500

    cal

---
+++ main.js
@@ -0,0 +1,96 @@
+const age = prompt("How old are you?");
+
+if  (age>=18) {
+    alert("Please enter");
+} else{
+    alert("Please enter after a few years")
+} 
+
+
+
+function main9() {
+    const input = document.getElementById('input1');
+    input.value +=1
+}
+function main10() {
+    const input = document.getElementById('input1');
+    input.value +=2
+}
+function main11() {
+    const input = document.getElementById('input1');
+    input.value +=3
+}
+function main5() {
+    const input = document.getElementById('input1');
+    input.value +=4
+}
+function main6() {
+    const input = document.getElementById('input1');
+    input.value +=5
+}
+function main7() {
+    const input = document.getElementById('input1');
+    input.value +=6
+}
+function main1() {
+    const input = document.getElementById('input1');
+    input.value +=7
+}
+function main2() {
+    const input = document.getElementById('input1');
+    input.value +=8
+}
+function main3() {
+    const input = document.getElementById('input1');
+    input.value +=9
+}
+function main4() {
+    const input = document.getElementById('input1');
+    input.value += "/"
+}
+function main8() {
+    const input = document.getElementById('input1');
+    input.value += "*"
+}
+function main12() {
+    const input = document.getElementById('input1');
+    input.value += "-"
+}
+function main16() {
+    const input = document.getElementById('input1');
+    input.value += "+"
+}
+function main13() {
+    const input = document.getElementById('input1');
+    input.value +=0
+}
+function main15() {
+ const input = document.getElementById("input1");
+ const javob = eval(input.value);
+input.value = javob 
+}
+function main01() {
+ let input = document.getElementById("input1");
+ 
+ input.value = " "
+}
+function main02() {
+ let input = document.getElementById("input1");
+
+    const kopay = Math.sqrt(input.value)
+    input.value = kopay
+}
+function main04() {
+    const input = document.getElementById('input1');
+    input.value += "**2"
+}
+
+function main03() {
+    const input = document.getElementById("input1");
+    const deletet = input.value.slice(0, input.value.length-1);
+     input.value = deletet;
+}
+
+
+
+
